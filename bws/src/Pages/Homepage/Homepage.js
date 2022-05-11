2b7b2c3637203c0678a655f7a45cd8fb71e342fb
elliott dombrowski <elliottroyal@me.com>
2022-05-11 00:32:22 -0500

    added boolean toggle to carousel btns

--- bws/src/Pages/Homepage/Homepage.js
+++ bws/src/Pages/Homepage/Homepage.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 import './styles.scss';
 import './query.scss';
 
+const controlLeft = <FontAwesomeIcon icon={faAngleLeft} className='fa-2x' />
+const controlRight = <FontAwesomeIcon icon={faAngleRight} className='fa-2x' />
+
 const Homepage = () => {
+  const [carousel, setCarousel] = useState(false);
+  console.log(carousel);
+
   return (
     <main className='homepage'>
       <section className='featured-block'>
+        <div className='control-wrapper'>
+
+          <div className='control-left'>
+            <i
+              onClick={() => setCarousel(prev => !prev)}
+            >
+              {controlLeft}
+            </i>
+          </div>
+
+          <div className='control-right'>
+            <i
+              onClick={() => setCarousel(prev => !prev)}
+            >
+              {controlRight}
+            </i>
+          </div>
+        </div>
+
         <div className='featured-header'></div>
 
         <div className='featured-img'>
@@ -32,5 +60,5 @@ const Homepage = () => {
     </main>
   );
 };
- 
+
 export default Homepage;
