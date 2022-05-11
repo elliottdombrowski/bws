fa3115ac052f79d39b1658ed72461249274c4260
elliott dombrowski <elliottroyal@me.com>
2022-05-10 23:21:25 -0500

    added file structure for homepage, added nav links

--- bws/src/Components/Header/Header.js
+++ bws/src/Components/Header/Header.js
@@ -6,9 +6,37 @@ import './query.scss';
 const Header = () => {
   return (
     <header>
+      <ul className='right'>
+        <li>
+          <a href='#'>
+            music
+          </a>
+        </li>
+
+        <li>
+          <a href='#'>
+            shows
+          </a>
+        </li>
+      </ul>
+
       <div className='logo'></div>
+
+      <ul className='right'>
+        <li>
+          <a href='#'>
+            merch
+          </a>
+        </li>
+
+        <li>
+          <a href='#'>
+            blog
+          </a>
+        </li>
+      </ul>
     </header>
   );
 };
- 
+
 export default Header;
