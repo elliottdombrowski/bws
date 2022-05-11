551fb26073fa16a3e9435d3f8745f5dbad7b6239
elliott dombrowski <elliottroyal@me.com>
2022-05-10 23:48:12 -0500

    too fast for spotify featured homepage

--- bws/src/Pages/Homepage/Homepage.js
+++ bws/src/Pages/Homepage/Homepage.js
@@ -6,7 +6,13 @@ import './query.scss';
 const Homepage = () => {
   return (
     <main className='homepage'>
+      <section className='featured-block'>
+        <div className='featured-header'></div>
 
+        <div className='featured-img'>
+          <img src={require('./too-fast.jpeg')} />
+        </div>
+      </section>
     </main>
   );
 };
