e3b1620580d05339ec453eb7b8af96573a8da101
elliott dombrowski <elliottroyal@me.com>
2022-05-10 23:00:51 -0500

    added glitch animation to logo

---
+++ bws/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+import './query.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='logo'></div>
+    </header>
+  );
+};
+ 
+export default Header;
