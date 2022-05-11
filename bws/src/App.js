69f8a0ab4d433dfcd4952cc860c52036d9921922
elliott dombrowski <elliottroyal@me.com>
2022-05-10 23:49:44 -0500

    default test footer render

--- bws/src/App.js
+++ bws/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Header from './Components/Header/Header';
 import Homepage from './Pages/Homepage/Homepage';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Homepage />
+      <Footer />
     </div>
   );
 }
---
+++ bws/src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+import './query.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+    </footer>
+  );
+}
+ 
+export default Footer;
