97007d930c8580200f96c44edf12b5c7228affca
elliott dombrowski <elliottroyal@me.com>
2022-05-10 23:27:22 -0500

    fixed background url logo positioning

---
+++ bws/src/Pages/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+import './query.scss';
+
+const Homepage = () => {
+  return (
+    <main className='homepage'>
+
+    </main>
+  );
+};
+ 
+export default Homepage;
