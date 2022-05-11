c37c0a901a05721c2cf408042e05bd954446cf36
elliott dombrowski <elliottroyal@me.com>
2022-05-10 23:53:46 -0500

    installing fontawesome

--- bws/src/Components/Footer/Footer.js
+++ bws/src/Components/Footer/Footer.js
@@ -6,6 +6,15 @@ import './query.scss';
 const Footer = () => {
   return (
     <footer>
+      <div className='media-icons'>
+
+      </div>
+
+      <div className='footer-contact'>
+        <a href='#'>
+          contact us.
+        </a>
+      </div>
     </footer>
   );
 }
